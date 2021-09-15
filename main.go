ddbc1a06e2457946b52415773594b7f47c064859
Maksym Soroka <msoroka@mindk.com>
2021-09-15 20:09:00 +0300

    do not remove first and last char

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	result := 0
 	if len(os.Args) > 1 && os.Args[1] != "" {
-		result = len(strings.Fields(os.Args[1][1 : len(os.Args[1])-1]))
+		result = len(strings.Fields(os.Args[1]))
 	}
 	fmt.Println(result)
 }
